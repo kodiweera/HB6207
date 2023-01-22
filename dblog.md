ee0eaa33bd14d2729549678536e1a5599a22a01b
Andy Connolly <andrew.c.connolly@dartmouth.edu>
2023-01-22 17:39:40 -0500

    This is a test of teh blogging system.

--- dblog.md
+++ dblog.md
@@ -1,3 +1,6 @@
+Date | Log Entry
+--- | ---
+Sun Jan 22 17:39:40 EST 2023 | This is a test of teh blogging system.
 |
 Sun Jan 22 17:29:25 EST 2023 This is a test of teh blogging system.
 |
--- dblog.sh
+++ dblog.sh
@@ -2,8 +2,8 @@
 
 msg=$@
 
-d="$( date ) $msg"
-{ echo "|"; echo ${d}; echo "|"; } > zero.txt
+d="$( date ) | $msg"
+{ echo "Date | Log Entry"; echo "--- | ---"; echo ${d}; } > zero.txt
 cat dblog.md >> zero.txt
 mv zero.txt dblog.md
 
