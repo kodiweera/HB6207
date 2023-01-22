afaf9a92e85529d9e39c60137206111b3fc57fed
Andy Connolly <andrew.c.connolly@dartmouth.edu>
2023-01-22 18:07:59 -0500

    msg

--- dblog.sh
+++ dblog.sh
@@ -3,13 +3,13 @@
 msg=$@
 
 { echo "Date | Log Entry"; echo ":--- | --- "; } > zero.txt
-echo $( date ) "|" $msg >> zero.txt 
+echo $( date ) "|" '`'$msg'`' >> zero.txt 
 
 cat dblog.md | grep 20  >> zero.txt
 mv zero.txt dblog.md
 
 git add .
-git commit -m "\`$msg\`"
+git commit -m "msg"
 git pull
 git push
 
