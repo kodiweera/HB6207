96519e1d51b65a518e5617429488f7c705f9afb0
Andy Connolly <andrew.c.connolly@dartmouth.edu>
2023-01-22 17:56:08 -0500

    And another

--- dblog.md
+++ dblog.md
@@ -1,4 +1,6 @@
 Date | Log Entry
 :--- | --- 
+Sun Jan 22 17:56:08 EST 2023 | And another
+:--- | --- 
 Sun Jan 22 17:52:30 EST 2023 |
 :--- | --- 
--- dblog.sh
+++ dblog.sh
@@ -2,8 +2,9 @@
 
 msg=$@
 
-d="$( date ) | `$msg`"
-{ echo "Date | Log Entry"; echo ":--- | --- "; echo ${d}; } > zero.txt
+{ echo "Date | Log Entry"; echo ":--- | --- "; } > zero.txt
+echo $( date ) "|" $msg >> zero.txt 
+
 cat dblog.md | grep -v "Log Entry" >> zero.txt
 mv zero.txt dblog.md
 
