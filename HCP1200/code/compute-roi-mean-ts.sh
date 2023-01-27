1a6eea6717f71743629d5b438e454caba654f388
andycon <andycconnolly@gmail.com>
2023-01-27 15:05:35 -0500

    removed abut

--- HCP1200/code/compute-roi-mean-ts.sh
+++ HCP1200/code/compute-roi-mean-ts.sh
@@ -4,10 +4,10 @@ mask=../data/CANlab_2018_combined_atlas_2mm.nii.gz
 
 date
 
-#mkdir ../data/${s}
-#aws s3 cp \
-#    s3://hcp-openaccess/HCP_1200/${s}/MNINonLinear/Results/${func}/${func}.nii.gz \
-#    ../data/${s}/${func}.nii.gz
+mkdir ../data/${s}
+aws s3 cp \
+    s3://hcp-openaccess/HCP_1200/${s}/MNINonLinear/Results/${func}/${func}.nii.gz \
+    ../data/${s}/${func}.nii.gz
 
 f=../data/${s}/${func}.nii.gz
 
@@ -15,10 +15,11 @@ f=../data/${s}/${func}.nii.gz
 for m in $( seq 2 17 )
 do
     3dmaskave -quiet -mask "${mask}<${m}>" ${f} > ../data/${s}/tmp
-    abut ../data/${s}/roi_avg_ts_${func} ../data/${s}/tmp > ../data/${s}/tmp2 
+    ./abut ../data/${s}/roi_avg_ts_${func} ../data/${s}/tmp > ../data/${s}/tmp2 
     mv ../data/${s}/tmp2 ../data/${s}/roi_avg_ts_${func}
     cat ../data/${s}/roi_avg_ts_${func}
 done
+rm ../data/${s}/${func}.nii.gz
 
 date
 
